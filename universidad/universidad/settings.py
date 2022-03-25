9098b80d28c2dd46b5d5dbe0fe33b448edadda58
Flor Perla <48634336+florperla@users.noreply.github.com>
2022-03-25 17:54:30 -0300

    Update settings.py

--- universidad/universidad/settings.py
+++ universidad/universidad/settings.py
@@ -123,7 +123,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "florrchuperla@gmail.com"
-EMAIL_HOST_PASSWORD = "fl0r3nc14."
+EMAIL_HOST_PASSWORD = ""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
